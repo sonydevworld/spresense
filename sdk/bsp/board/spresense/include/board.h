969388c04bf00fc834b77e5d4b6c886dd837dae9
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2018-11-14 19:51:24 +0900

    Fix assignment of power switch to bluetooth chip

--- sdk/bsp/board/spresense/include/board.h
+++ sdk/bsp/board/spresense/include/board.h
@@ -162,8 +162,8 @@ enum board_power_device {
   POWER_AUDIO_AVDD      = PMIC_GPO(1),
   POWER_AUDIO_MUTE      = PMIC_GPO(6),
   POWER_IMAGE_SENSOR    = PMIC_GPO(4) | PMIC_GPO(5) | PMIC_GPO(7),
-  POWER_BTBLE           = PMIC_GPO(3),
 
+  POWER_BTBLE           = PMIC_NONE,
   POWER_SENSOR          = PMIC_NONE,
   POWER_EMMC            = PMIC_NONE,
 };
