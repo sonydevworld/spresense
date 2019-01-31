ba1f51d4f6d2085077fb3f1c231f1ef2ccd769ba
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2019-01-31 20:34:36 +0900

    Add envsetup.sh for enabling user application environment
    Usage:
     $ source tools/envsetup.sh

---
+++ sdk/tools/envsetup.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+############################################################################
+# tools/envsetup.sh
+#
+#   Copyright 2019 Sony Semiconductor Solutions Corporation
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+# 3. Neither the name of Sony Semiconductor Solutions Corporation nor
+#    the names of its contributors may be used to endorse or promote
+#    products derived from this software without specific prior written
+#    permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+############################################################################
+
+CURRENT_DIR=`pwd`
+SCRIPT_NAME=`readlink -e "${BASH_SOURCE[0]}"`
+SCRIPT_DIR=`dirname "$SCRIPT_NAME"`
+
+# Create Spresense home environmet with mkapp
+function create_spresense_home()
+{
+    echo "Creating application directory into ${SPRESENSE_HOME}"
+    ${SCRIPT_DIR}/mkappsdir.py -s ${SPRESENSE_HOME} "User application"
+}
+
+# Set <user home>/Spresense to SPRESENSE_HOME
+SPRESENSE_HOME=${HOME}/Spresense
+
+# Set repository root to SPRESENSE_SDK
+SPRESENSE_SDK=${SCRIPT_DIR}
+
+if [ ! -d ${SPRESENSE_HOME} ]; then
+    echo "Warning: Spresense user application directory is not exists."
+    echo "         Please run"
+    echo "         $ create_spresense_home"
+fi
+
+# Export parameters
+export SPRESENSE_HOME
+export SPRESENSE_SDK
+
+# Export parameters into configuration file
+echo "SPRESENSE_HOME=${SPRESENSE_HOME}" > ${HOME}/.spresense_env
+echo "SPRESENSE_SDK=${SPRESENSE_SDK}" >> ${HOME}/.spresense_env
+
+if [ -f ${SPRESENSE_HOME}/Application.mk ]; then
+    # Echo result
+    echo "Set user application root directory to ${SPRESENSE_HOME}."
+    echo "You can put application directory into ${SPRESENSE_HOME}"
+else
+    if [ -d ${SPRESENSE_HOME} ]; then
+        # Echo warning
+        echo "Warning: Your environment(${SPRESENSE_HOME}) doesn't have makefiles."
+        echo "         Please move ${SPRESENSE_HOME} to other place and run"
+        echo "         $ create_spresense_home"
+    fi
+fi
+
