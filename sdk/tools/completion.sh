1a08094059fde065830fa8b8690cee76cf27d554
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2019-03-19 13:20:55 +0900

    Separate completion script for decouple
    Separate script for use completion in local

--- sdk/tools/completion.sh
+++ sdk/tools/completion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ############################################################################
-# tools/envsetup.sh
+# tools/completion.sh
 #
 #   Copyright 2019 Sony Semiconductor Solutions Corporation
 #
@@ -34,70 +34,42 @@
 #
 ############################################################################
 
-CURRENT_DIR=`pwd`
-SCRIPT_NAME=`readlink -e "${BASH_SOURCE[0]}"`
-SCRIPT_DIR=`dirname "$SCRIPT_NAME"`
-
 ############################################################################
 # Public function                                                          #
 ############################################################################
 
-# Create Spresense home environmet with mkapp
-function create_spresense_home()
-{
-    echo "Creating application directory into ${SPRESENSE_HOME}"
-    ${SCRIPT_DIR}/mkappsdir.py -s ${SPRESENSE_HOME} "User application"
+# TAB completion for ./tools/config.py
+function _spresense_config_completion() {
+	local cur prev cword
+
+	# get command line arguments
+	_get_comp_words_by_ref -n : cur prev cword
+
+	if [ "${prev}" == "-d" -o "${prev}" == "--dir" ]; then
+		# If use '-d' or '--dir' option, use filename completion
+		compopt -o nospace
+		COMPREPLY=($(compgen -d -- "${cur}" | sed 's#$#/#g'))
+	else
+		compopt +o nospace
+		if [ "${cur:0:1}" == "-" ]; then
+			# For option prediction
+			SOPT=`${COMP_WORDS[@]} -h | grep -oE "\--[a-zA-Z]+"`
+			LOPT=`${COMP_WORDS[@]} -h | grep -oE "^[ ]+-[a-zA-Z]{1}"  | tr -d " "`
+			LIST="${SOPT} ${LOPT}"
+		else
+			LIST=`${COMP_WORDS[@]} -l | tail -n +2 | tr -d "\t"`
+		fi
+		COMPREPLY=($(compgen -W "${LIST}" -- "${cur}"))
+	fi
 }
-
-# Add configuration command behalf of tools/config.py
-function spresense_config() {
-	cd ${SPRESENSE_SDK}/sdk
-	./tools/config.py $@
-}
-
-############################################################################
-# Public parameter definition                                              #
-############################################################################
-
-# Set <user home>/Spresense to SPRESENSE_HOME
-export SPRESENSE_HOME=${HOME}/Spresense
-
-# Set repository root to SPRESENSE_SDK
-export SPRESENSE_SDK=$(dirname $(dirname ${SCRIPT_DIR}))
 
 ############################################################################
 # Environment setup                                                        #
 ############################################################################
 
 #
-# User application setup
-#
-
-if [ ! -d ${SPRESENSE_HOME} ]; then
-    echo "Warning: Spresense user application directory is not exists."
-    echo "         Please run"
-    echo "         $ create_spresense_home"
-fi
-
-# Export parameters into configuration file
-echo "SPRESENSE_HOME=${SPRESENSE_HOME}" > ${HOME}/.spresense_env
-echo "SPRESENSE_SDK=${SPRESENSE_SDK}" >> ${HOME}/.spresense_env
-
-if [ -f ${SPRESENSE_HOME}/Application.mk ]; then
-    # Echo result
-    echo "Set user application root directory to ${SPRESENSE_HOME}."
-    echo "You can put application directory into ${SPRESENSE_HOME}"
-else
-    if [ -d ${SPRESENSE_HOME} ]; then
-        # Echo warning
-        echo "Warning: Your environment(${SPRESENSE_HOME}) doesn't have makefiles."
-        echo "         Please move ${SPRESENSE_HOME} to other place and run"
-        echo "         $ create_spresense_home"
-    fi
-fi
-
-#
 # TAB completion
 #
 
-source ${SCRIPT_DIR}/completion.sh
+complete -F _spresense_config_completion tools/config.py ./tools/config.py spresense_config
+
