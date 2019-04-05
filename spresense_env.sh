2d4ac4972e2952d4a42404feeab42b316a15c25c
Kalle Komierowski <sa7bkk@gmail.com>
2019-04-05 16:56:07 +0200

    Updated Docker to build latest 1.2.0
    * Fixed script to make it easier to use.

--- spresense_env.sh
+++ spresense_env.sh
@@ -4,7 +4,8 @@ echo "This script has to be sourced."
 echo ". ./spresense_env.sh or source spresense_env.sh"
 echo " "
 docker pull sa7bkk/sdk-env-minimal
-alias spresense='docker run --rm -it -u `id -u`:`id -g`  -v `pwd`:/spresense -w /spresense/sdk sa7bkk/sdk-env-minimal'
+pwd=$(pwd)
+alias spresense='docker run --rm -it -u `id -u`:`id -g`  -v $pwd:/spresense -w /spresense/sdk sa7bkk/sdk-env-minimal'
 if [[ $PS1 !=  *$PROMT_PREFIX* ]]; then
 	PS1=$PROMT_PREFIX$PS1
 fi
