0327de78f35f3815885d3dc67f6d8580bb782fc9
Karl Komierowski <karl.komierowski@sony.com>
2019-06-13 14:19:55 +0200

    Updated Dockerfile
    * Using the provided script for tool installation.

--- spresense_env.sh
+++ spresense_env.sh
@@ -6,7 +6,7 @@ echo ". ./spresense_env.sh or source spresense_env.sh"
 echo " "
 docker pull $REPO
 pwd=$(pwd)
-alias spresense='docker run --rm -it -u `id -u`:`id -g`  -v $pwd:/spresense -w /spresense/sdk $REPO'
+alias spresense='docker run --rm -it -u `id -u`:`id -g`  -v '"$pwd:/spresense -w /spresense/sdk $REPO"
 if [[ $PS1 !=  *$PROMT_PREFIX* ]]; then
 	PS1=$PROMT_PREFIX$PS1
 fi
