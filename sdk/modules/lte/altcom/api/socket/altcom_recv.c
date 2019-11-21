0182a0472570513616cce2e92b346604978da7e2
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2019-11-21 16:41:24 +0900

    Fix bug that blocked even if MSG_DONTWAIT is set
    Fix bug that blocked even if MSG_DONTWAIT is specified
    in flags of the following functions.
    
     - recv()
     - recvfrom()
     - send()
     - sendto()

--- sdk/modules/lte/altcom/api/socket/altcom_recv.c
+++ sdk/modules/lte/altcom/api/socket/altcom_recv.c
@@ -217,7 +217,7 @@ int altcom_recv(int sockfd, void *buf, size_t len, int flags)
   req.len    = len;
   req.flags  = flags;
 
-  if (fsock->flags & ALTCOM_O_NONBLOCK)
+  if ((fsock->flags & ALTCOM_O_NONBLOCK) || (flags & ALTCOM_MSG_DONTWAIT))
     {
       /* Check recv buffer is available */
 
