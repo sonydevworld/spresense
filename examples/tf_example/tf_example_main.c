55d5610248d62b71791da5d0b67233c6330964a2
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2022-02-21 21:32:36 +0100

    externals/tensorflow: Fix warnings

--- examples/tf_example/tf_example_main.c
+++ examples/tf_example/tf_example_main.c
@@ -40,6 +40,8 @@
 #include <nuttx/config.h>
 #include <stdio.h>
 
+#include "tensorflow/lite/micro/spresense/debug_log_callback.h"
+
 #ifndef CONFIG_EXTERNALS_TENSORFLOW
 #error "tf_example require external library of tensorflow lite micro." \
   " Please enable it as set EXTERNALS_TENSORFLOW in configuration."
