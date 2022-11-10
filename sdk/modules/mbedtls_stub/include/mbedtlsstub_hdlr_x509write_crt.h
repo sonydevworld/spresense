e2cd9475a38969dccb36ad063c53c3db84062c4d
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2022-11-10 12:05:10 +0900

    modules/mbedtls_stub: Split the mbedtlsstub_hdlr.c file
    Split mbedtlsstub_hdlr.c into the following files to improve maintainability.
    - mbedtlsstub_hdlr_ssl.c
    - mbedtlsstub_hdlr_ssl_conf.c
    - mbedtlsstub_hdlr_ssl_session.c
    - mbedtlsstub_hdlr_x509_crt.c
    - mbedtlsstub_hdlr_pk.c
    - mbedtlsstub_hdlr_ctr_drbg.c
    - mbedtlsstub_hdlr_entropy.c
    - mbedtlsstub_hdlr_cipher.c
    - mbedtlsstub_hdlr_md.c
    - mbedtlsstub_hdlr_base64.c
    - mbedtlsstub_hdlr_sha1.c
    - mbedtlsstub_hdlr_bignum.c
    - mbedtlsstub_hdlr_x509_csr.c
    - mbedtlsstub_hdlr_x509write_crt.c
    - mbedtlsstub_hdlr_rsa.c
    - mbedtlsstub_hdlr_ssl_srtp.c
    - mbedtlsstub_hdlr_x509_dn.c

---
+++ sdk/modules/mbedtls_stub/include/mbedtlsstub_hdlr_x509write_crt.h
@@ -0,0 +1,146 @@
+/****************************************************************************
+ * modules/mbedtls_stub/include/mbedtlsstub_hdlr_x509write_crt.h
+ *
+ *   Copyright 2022 Sony Semiconductor Solutions Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of Sony Semiconductor Solutions Corporation nor
+ *    the names of its contributors may be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef __MODULES_MBEDTLS_STUB_INCLUDE_MBEDTLSSTUB_HDLR_X509WRITE_CRT_H
+#define __MODULES_MBEDTLS_STUB_INCLUDE_MBEDTLSSTUB_HDLR_X509WRITE_CRT_H
+
+ /****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+int32_t mbedtlsstub_x509writecrtinit_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtfree_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtder_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtpem_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtsubkey_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtissuerkey_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtsubname_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtissuername_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtver_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtmdalg_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtserial_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtvalidity_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtconst_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtsubid_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtauthid_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtkeyusage_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtnscerttype_pkt_compose(FAR void **arg,
+                              size_t arglen, uint8_t altver, FAR uint8_t *pktbuf,
+                              const size_t pktsz, FAR uint16_t *altcid);
+int32_t mbedtlsstub_x509writecrtinit_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtfree_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtder_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtpem_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtsubkey_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtissuerkey_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtsubname_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtissuername_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtver_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtmdalg_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtserial_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtvalidity_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtconst_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtsubid_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtauthid_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtkeyusage_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+int32_t mbedtlsstub_x509writecrtnscerttype_pkt_parse(FAR struct alt1250_dev_s *dev, FAR uint8_t *pktbuf,
+                              size_t pktsz, uint8_t altver, FAR void **arg,
+                              size_t arglen, FAR uint64_t *bitmap);
+
+#endif /* __MODULES_MBEDTLS_STUB_INCLUDE_MBEDTLSSTUB_HDLR_X509WRITE_CRT_H */
