3faf426701ea3d370f0d816bcb1c88e05556f9dc
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2024-10-18 18:04:49 +0900

    examples/bluetooth_le_central: Fix uninitialized reference
    Fix bug that accesses unused member variable on characteristic read.
    Detected by CodeSonar 691384.55063080

--- examples/bluetooth_le_central/bluetooth_le_central_main.c
+++ examples/bluetooth_le_central/bluetooth_le_central_main.c
@@ -457,7 +457,7 @@ static void on_read(uint16_t conn_handle, struct ble_gatt_char_s *ble_gatt_char)
 
   g_read_datalen = ble_gatt_char->value.length;
   memcpy(g_read_data, ble_gatt_char->value.data, g_read_datalen);
-  g_charrd_result = ble_gatt_char->status;
+  g_charrd_result = (g_read_datalen > 0) ? BT_SUCCESS : BT_FAIL;
 }
 
 static void on_notify(uint16_t conn_handle, struct ble_gatt_char_s *ble_gatt_char)
