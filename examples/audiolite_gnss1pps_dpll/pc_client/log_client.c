b91962090406b605e775a0f21c814dfb86890f59
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2025-02-27 14:18:55 +0900

    examples/audiolite_gnss1pps_dpll: Add Licence description
    Licence description on log_client.c is added.

--- examples/audiolite_gnss1pps_dpll/pc_client/log_client.c
+++ examples/audiolite_gnss1pps_dpll/pc_client/log_client.c
@@ -1,3 +1,42 @@
+/****************************************************************************
+ * examples/audiolite_gnss1pps_dpll/pc_tool/log_client.c
+ *
+ *   Copyright 2025 Sony Semiconductor Solutions Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of Sony Semiconductor Solutions Corporation nor
+ *    the names of its contributors may be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
@@ -13,6 +52,10 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
 #define SERVER_PORT_NUM (12345)     /* Server Port number */
 
 #define SAMPLES  (1024)
@@ -20,6 +63,10 @@
 #define DATASIZE (2)
 #define SAMPLE_CH (SAMPLES * DATASIZE)
 #define DATA_MAX  (SAMPLE_CH * CHNUM)
+
+/****************************************************************************
+ * Private Types
+ ****************************************************************************/
 
 struct lr_data_s
 {
@@ -34,11 +81,19 @@ union sample_data_u
   struct lr_data_s samp_lr[SAMPLES];
 };
 
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
 static union sample_data_u g_rcvdata;
 static int en_print = 1;
 static int planner_lr = 1;
 
 static char data_csv[1024];
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
 
 static void dump_top_data(int sz, FILE *fp)
 {
@@ -156,6 +211,10 @@ static int connect_server(const char *ip)
 
   return sock;
 }
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
 
 int main(int argc, char **argv)
 {
